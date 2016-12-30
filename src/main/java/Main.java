864857baa648b3dc3be36afb964abaca504e8d8e
jiapeng <jiapeng@ekexiu.com>
2016-12-30 10:21:24 +0800

    -------

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,5 +11,11 @@ public class Main {
     public static void maind(String[] args)throws Exception{
         System.out.println(333333333);
     }
+    
+    
+    public static void sss(String[] args)throws Exception{
+        System.out.println(333333333);
+    }
+
 }
 
