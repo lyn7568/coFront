e6bfcfcb3a525649c816bcf24d8caf2d4b80faf2
jiapeng <jiapeng@ekexiu.com>
2016-12-30 10:04:45 +0800

    dfved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args)throws Exception{
         System.out.println(333333333);
     }
+    public static void maind(String[] args)throws Exception{
+        System.out.println(333333333);
+    }
 }
 
