7049cc6f462fd57ff532611c07efbe32fcda0178
XMTT <702030452@qq.com>
2017-04-20 17:52:20 +0800

    pictable

--- js/model/sys/pictable/index.js
+++ js/model/sys/pictable/index.js
@@ -1,28 +1,38 @@
-/**
- * Created by TT on 2017/4/19.
- */
 ;
 spa_define(function () {
     return $.use(["spa", "code", "form", "util", "dict"], function (spa, code, form, util, dict) {
         return {
             main: function () {
-                var root = spa.findInMain(".sys_chart_index");
-                var qf = form.build(root.find(".queryForm"));
-                var queryBtn = root.find(".queryForm .icon-search");
-                var myChart1 = echarts.init(document.getElementById('type'));
-                var tableData = {data: []}, tableData2 = {data: []},
-                    allData = [];
+                var root = spa.findInMain(".sys_pictable_index");
+                var cr = code.parseCode(root.find(".dt-tpl-2"));
+                var myChart = echarts.init(document.getElementById('type'));
 
-                var load = function () {
-                    util.get("../ajax/operation/statist/user", null, function (data) {
-                        allData = data || [];
-                        query();
-                        query2();
+                var dynUser = {ready: true};
+
+                util.get("../ajax/sys/user/all",null,function(data){
+                    var items =dynUser.items = [];
+                    data.forEach(function(item){
+                        items.push({code:item.id,caption:item.name});
+                    });
+                    root.find(".dyn-select").attr("defVal",util.data("loginUser").id);
+                    dynUser.formRef = form.build(root.find(".dt-form"),{name:dynUser});
+                });
+
+                cr.shell("count", function (env) {
+                    var v = env.cd[this.k];
+                    if (v === 0) {
+                        return 0;
+                    }
+                    return v;
+                });
+
+                var loadChart = function () {
+                    util.get("../ajax/sys/user/pictable/"+util.data("loginUser").id,null,function (data) {
+                        var colors = ['#c23531', '#82C8FA'];
                         var option = {
-
                             color: colors,
                             title: {
-                                text: '用户流量统计',
+                                text: '个人业绩统计',
                                 subtext: '最近7天'
                             },
                             tooltip: {
@@ -32,7 +42,7 @@ spa_define(function () {
                                 data: ['新增用户', '新增激活用户']
                             },
                             xAxis: {
-                                data: allData.map(function (item) {
+                                data: data.map(function (item) {
                                     return item.createTime;
                                 }),
                                 boundaryGap: false
@@ -70,25 +80,97 @@ spa_define(function () {
                             series: [{
                                 name: '新增用户',
                                 type: 'line',
-                                data: allData.map(function (item) {
+                                data: data.map(function (item) {
                                     return item.znum;
                                 }),
                             }, {
                                 name: '新增激活用户',
                                 type: 'line',
-                                data: allData.map(function (item) {
+                                data: data.map(function (item) {
                                     return item.jnum;
                                 })
                             }
                             ]
                         };
-                        myChart1.setOption(option1);
-                    }, {});
+                        cr.val(data);
+                        myChart.setOption(option);
+                    })
                 };
-            },
-            mainDestory: function () {
+                root.find(".opt-query").on("click", function () {
+                    $.util.get("../ajax/sys/user/pictable/" + dynUser.formRef.val().name, null, function (data) {
+                        var colors = ['#c23531', '#82C8FA'];
+                        var option = {
+                            color: colors,
+                            title: {
+                                text: '个人业绩统计',
+                                subtext: '最近7天'
+                            },
+                            tooltip: {
+                                trigger: 'axis'
+                            },
+                            legend: {
+                                data: ['新增用户', '新增激活用户']
+                            },
+                            xAxis: {
+                                data: data.map(function (item) {
+                                    return item.createTime;
+                                }),
+                                boundaryGap: false
+                            },
+                            yAxis: {
+                                splitLine: {
+                                    show: false
+                                },
+                                boundaryGap: false,
+                                type: "value",
+                                interval: 1,
+                                min: 0
+                            },
+                            toolbox: {
+                                show: true,
+                                feature: {
+                                    dataZoom: {
+                                        yAxisIndex: 'none'
+                                    },
+                                    dataView: {readOnly: true},
+                                    magicType: {type: ['line', 'bar']},
+                                    restore: {},
+                                    saveAsImage: {}
+                                }
+                            },
+                            dataZoom: [{
+                                startValue: ""
+                            }, {
+                                type: 'inside'
+                            }],
+                            visualMap: {
+                                top: 10,
+                                right: 10,
+                            },
+                            series: [{
+                                name: '新增用户',
+                                type: 'line',
+                                data: data.map(function (item) {
+                                    return item.znum;
+                                }),
+                            }, {
+                                name: '新增激活用户',
+                                type: 'line',
+                                data: data.map(function (item) {
+                                    return item.jnum;
+                                })
+                            }
+                            ]
+                        };
+                        myChart.setOption(option);
+                        cr.val(data);
+                    });
+                });
+                cr.listen(dict.doTransfer);
+                loadChart();
+
 
             }
-        };
+        }
     });
 });
