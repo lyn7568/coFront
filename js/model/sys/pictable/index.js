2975adcab823de0383397847174fb7ceaea56421
XMTT <702030452@qq.com>
2017-04-20 10:27:39 +0800

    ..

--- js/model/sys/pictable/index.js
+++ js/model/sys/pictable/index.js
@@ -1,9 +1,6 @@
 /**
  * Created by TT on 2017/4/19.
  */
-/**
- * Created by TT on 2017/4/6.
- */
 ;
 spa_define(function () {
     return $.use(["spa", "code", "form", "util", "dict"], function (spa, code, form, util, dict) {
@@ -11,64 +8,11 @@ spa_define(function () {
             main: function () {
                 var root = spa.findInMain(".sys_chart_index");
                 var qf = form.build(root.find(".queryForm"));
-                var cr = code.parseCode(root.find(".dt-tpl"));
-                var cr2 = code.parseCode(root.find(".dt-tpl-2"));
                 var queryBtn = root.find(".queryForm .icon-search");
                 var myChart1 = echarts.init(document.getElementById('type'));
                 var tableData = {data: []}, tableData2 = {data: []},
                     allData = [];
 
-                cr.shell("userType", function (env) {
-                    var v = env.cd[this.k];
-                    return v === 1 ? "专家用户" : (v === 0 ? "普通用户" : "");
-                });
-                cr2.shell("userRole",function (env) {
-                    var v = env.cd[this.k];
-                    if (v === 1) {
-                        return "科研工作者";
-                    }
-                    if (v === 2) {
-                        return "在企人员";
-                    }
-                    if (v === 3) {
-                        return "在校生";
-                    }
-                    return "其他身份";
-                });
-                var query = function () {
-                    var val = qf.item("qn").get();
-                    if (val) {
-                        var td = tableData.data = [];
-                        if (allData.byType != null) {
-                            for (var i = 0; i < allData.byType.length; ++i) {
-                                var item = allData.byType[i];
-                                if (item && item.name && item.name.indexOf(val) >= 0) {
-                                    td.push(item);
-                                }
-                            }
-                        }
-                    } else {
-                        tableData.data = allData.byType;
-                    }
-                    cr.val(tableData.data);
-                };
-                var query2 = function () {
-                    var val = qf.item("qn2").get();
-                    if (val) {
-                        var td = tableData2.data = [];
-                        if (allData.byRole != null) {
-                            for (var i = 0; i < allData.byRole.length; ++i) {
-                                var item = allData.byRole[i];
-                                if (item && item.name && item.name.indexOf(val) >= 0) {
-                                    td.push(item);
-                                }
-                            }
-                        }
-                    } else {
-                        tableData2.data = allData.byRole;
-                    }
-                    cr2.val(tableData2.data);
-                };
                 var load = function () {
                     util.get("../ajax/operation/statist/user", null, function (data) {
                         allData = data || [];
@@ -141,11 +85,6 @@ spa_define(function () {
                         myChart1.setOption(option1);
                     }, {});
                 };
-                queryBtn.on("click", query());
-                cr.listen(dict.doTransfer);
-                root.on("click",".opt-new",function () {
-                    load();
-                });
             },
             mainDestory: function () {
 
