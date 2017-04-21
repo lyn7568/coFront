5923473d497b4cc7c6cb5ef01df7d28a78a6b9a3
XMTT <702030452@qq.com>
2017-04-21 10:54:35 +0800

    add  UserEdit

---
+++ js/model/sys/user/edit.js
@@ -0,0 +1,25 @@
+;
+spa_define(function(spa) {
+
+	return {
+		modal: function(data) {
+			var modalCtn = spa.getLastModalCtn();
+			var root = modalCtn = modalCtn.children();
+			var form = root.find(".newForm").form();
+			root.find(".modal-ctrl .icon-times").on("click", function() {
+				spa.closeModal();
+			});
+			var handler = data.hand;
+			var closeThis = function() {
+				spa.closeModal();
+				if(handler) {
+					handler();
+				}
+			};
+			form.val(data.data);
+			root.find(".opt-save").on("click", function() {
+				form.doPut("../ajax/sys/user", closeThis, {});
+			});
+		}
+	};
+});
