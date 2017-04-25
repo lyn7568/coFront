25a7684ae4978700cc6b682b41387ab4e421807c
XMTT <702030452@qq.com>
2017-04-25 11:21:33 +0800

    客户姓名跳转

--- js/model/sys/customer/allindex.js
+++ js/model/sys/customer/allindex.js
@@ -1,23 +1,30 @@
 ;
-spa_define(function(){
-	return $.use(["spa","pagedatagrid","util"],function(spa,pdgf,util){
-		return{
-			main: function() {				
-				var root = spa.findInMain(".sys_customer_allindex");
-				var pdg = pdgf.build(root);
-				pdg.code.shell("showDay", function(env) {
-					if(env.cd && env.cd[this.k]) {
-						var day = env.cd[this.k];
-						return day.substring(0, 4) + "-" + day.substring(4, 6) + "-" + day.substring(6, 8);
-					}
-					return "";
-				});
-				pdg.code.listen($.dict.doTransfer);
-				root.find(".opt-query").on("click", function() {
-					pdg.load();
-				});
-				pdg.load();
-			}
-		};
-	});
+spa_define(function () {
+    return $.use(["spa", "pagedatagrid", "util"], function (spa, pdgf, util) {
+        return {
+            main: function () {
+                var root = spa.findInMain(".sys_customer_allindex");
+                var pdg = pdgf.build(root);
+                pdg.code.shell("showDay", function (env) {
+                    if (env.cd && env.cd[this.k]) {
+                        var day = env.cd[this.k];
+                        return day.substring(0, 4) + "-" + day.substring(4, 6) + "-" + day.substring(6, 8);
+                    }
+                    return "";
+                });
+                pdg.code.listen($.dict.doTransfer);
+                root.find(".opt-query").on("click", function () {
+                    pdg.load();
+                });
+                pdg.load();
+                var event = function () {
+                    root.find(".table-opt i.name").on("click", function () {
+                        var professorId = $(this).parent().attr("professorId");
+                        window.open('http://www.ekexiu.com/information-brow.html?professorId=' + professorId);
+                    });
+                };
+                pdg.code.listen(event);
+            }
+        };
+    });
 });
