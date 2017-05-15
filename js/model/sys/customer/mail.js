5d65a56d048664dd5d9f93f2a1fe0f3403291d4d
XMTT <702030452@qq.com>
2017-05-15 15:35:59 +0800

    邮件发送

--- js/model/sys/customer/mail.js
+++ js/model/sys/customer/mail.js
@@ -8,19 +8,33 @@ spa_define(function () {
             modal: function (data) {
                 var root = spa.findInModal(".sys_customer_myindex_mail");
                 var form = fb.build(root.find(".newForm"));
-                form.val({emails: data.emails, mobilePhones: data.phones, inviteCodes: data.inviteCodes,name:data.names});
+                form.val({
+                    emails: data.emails,
+                    mobilePhones: data.phones,
+                    inviteCodes: data.inviteCodes,
+                    name: data.names,
+                    names:data.names
+                });
                 var send = root.find(".opt-send"),
                     save = function () {
                         var boolean = false;
+                        var empty = 0;
                         for (var i = 0; i < form.val().emails.length; i++) {
                             var item = form.val().emails[i];
                             if (item != "") {
                                 boolean = true
-                            }
+                            }else empty++;
                         }
                         if (boolean) {
                             form.doPost("../ajax/sendmail", function (data) {
-                                util.alert("您成功发送" + data + "封邮件！");
+                                var fail = form.val().emails.length - data.count - empty;
+                                for (var i = 0; i < form.val().emails.length; i++) {
+                                    var item = form.val().emails[i];
+                                    if (item == "") {
+                                        data.names += "，"+form.val().names[i];
+                                    }
+                                }
+                                util.alert("您发送成功" + data.count + "封邮件！" + empty + "个用户邮箱为空。" + fail + "封邮件发送失败；失败用户：" + data.names + "；失败邮箱：" + data.emails);
                             }, {});
                         } else {
                             util.alert("请选择收件人（收件人邮箱和邀请码不能为空）！");
