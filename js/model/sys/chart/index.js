96994955e109d563d3825f7f1337cab37a3183bd
XMTT <702030452@qq.com>
2017-06-02 17:30:53 +0800

    图表

--- js/model/sys/chart/index.js
+++ js/model/sys/chart/index.js
@@ -11,8 +11,8 @@ spa_define(function () {
                 var cr2 = code.parseCode(root.find(".dt-tpl-2"));
                 var cr3 = form.build(root.find(".newForm"));
                 var myChart = echarts.init(document.getElementById('type'));
-                var tableData = {data: []}, tableData2 = {data: []},tableData3 = {data: []}
-                    allData = [];
+                var tableData = {data: []}, tableData2 = {data: []}, tableData3 = {data: []}
+                allData = [];
 
                 cr.shell("userType", function (env) {
                     var v = env.cd[this.k];
@@ -20,14 +20,32 @@ spa_define(function () {
                 });
                 cr2.shell("userRole", function (env) {
                     var v = env.cd[this.k];
+                    if (v === 0) {
+                        return "普通用户";
+                    }
                     if (v === 1) {
                         return "科研工作者";
                     }
                     if (v === 2) {
-                        return "在企人员";
+                        return "企业高管";
                     }
                     if (v === 3) {
                         return "在校生";
+                    }
+                    if (v === 4) {
+                        return "技术人员";
+                    }
+                    if (v === 5) {
+                        return "HR/猎头";
+                    }
+                    if (v === 6) {
+                        return "销售人员";
+                    }
+                    if (v === 7) {
+                        return "投资方";
+                    }
+                    if (v === 8) {
+                        return "咨询顾问";
                     }
                     return "其他身份";
                 });
@@ -44,7 +62,7 @@ spa_define(function () {
                     cr2.val(tableData2.data);
                 };
                 var query3 = function () {
-                    if (allData!=null) {
+                    if (allData != null) {
                         tableData3.data = allData;
                     }
                     cr3.val(tableData3.data);
@@ -85,52 +103,74 @@ spa_define(function () {
                                     type: 'pie',
                                     radius: '55%',
                                     center: ['25%', '60%'],
-                                    data: [
-                                        {value: 56, name: '在企人员'},
-                                        {value: 1465, name: '科研工作者'},
-                                    ]
+                                    data: Type()
                                 },
                                 {
                                     name: '用户角色',
                                     type: 'pie',
                                     radius: '55%',
                                     center: ['75%', '60%'],
-                                    data: [
-                                        {value: 56, name: '在企人员'},
-                                        {value: 1465, name: '科研工作者'},
-                                        {value: 10, name: '在校生'},
-                                        {value: 19, name: '其他'},
-                                    ]
-
+                                    data: Role()
                                 }
                             ]
                         };
-                        var showType = function (data) {
-                            if (data.code === 1) {
-                                return "专家用户";
+
+                        function Type() {
+                            var t = [];
+                            var showType = function (data) {
+                                if (data.code === 1) {
+                                    return "专家用户";
+                                }
+                                return "普通用户";
+                            };
+                            for (var i = 0; i < allData.byType.length; i++) {
+                                // option.series[0].data[i].value = allData.byType[i].num;
+                                // option.series[0].data[i].name = showType(allData.byType[i]);
+                                t.push({value: allData.byType[i].num, name: showType(allData.byType[i])});
                             }
-                            return "普通用户";
-                        };
-                        var showRole = function (data) {
-                            if (data.code === 1) {
-                                return "科研工作者";
-                            }
-                            if (data.code === 2) {
-                                return "在企人员";
-                            }
-                            if (data.code === 3) {
-                                return "在校生";
-                            }
-                            return "其他身份";
+                            return t
                         }
-                        for (var i = 0; i < allData.byType.length; i++) {
-                            option.series[0].data[i].value = allData.byType[i].num;
-                            option.series[0].data[i].name = showType(allData.byType[i]);
+
+                        function Role() {
+                            var r = [];
+                            var showRole = function (data) {
+                                if (data.code === 0) {
+                                    return "普通用户";
+                                }
+                                if (data.code === 1) {
+                                    return "科研工作者";
+                                }
+                                if (data.code === 2) {
+                                    return "企业高管";
+                                }
+                                if (data.code === 3) {
+                                    return "在校生";
+                                }
+                                if (data.code === 4) {
+                                    return "技术人员";
+                                }
+                                if (data.code === 5) {
+                                    return "HR/猎头";
+                                }
+                                if (data.code === 6) {
+                                    return "销售人员";
+                                }
+                                if (data.code === 7) {
+                                    return "投资方";
+                                }
+                                if (data.code === 8) {
+                                    return "咨询顾问";
+                                }
+                                return "其他身份";
+                            };
+                            for (var i = 0; i < allData.byRole.length; i++) {
+                                // option.series[1].data[i].value = allData.byRole[i].num;
+                                // option.series[1].data[i].name = showRole(allData.byRole[i]);
+                                r.push({value: allData.byRole[i].num, name: showRole(allData.byRole[i])});
+                            }
+                            return r;
                         }
-                        for (var i = 0; i < allData.byRole.length; i++) {
-                            option.series[1].data[i].value = allData.byRole[i].num;
-                            option.series[1].data[i].name = showRole(allData.byRole[i]);
-                        }
+
                         myChart.setOption(option);
                     }, {});
                 };
