e275c0392ddc7cec34ed7e57b049192529d87021
XMTT <702030452@qq.com>
2017-11-30 13:53:48 +0800

    文章统计图

--- js/model/sys/article/viewcount.js
+++ js/model/sys/article/viewcount.js
@@ -25,10 +25,10 @@ spa_define(function () {
 
                 var caption = function (dicts, code) {
                         var items = dict.get(dicts);
-                        var cp =  dict.getCap(items, code.toString());
+                        var cp = dict.getCap(items, code.toString());
                         if (cp) {
                             return cp;
-                        }else {
+                        } else {
                             return "不可翻译的";
                         }
                         // return dict.get(dicts)[code].caption;
@@ -43,7 +43,7 @@ spa_define(function () {
                             var $e = $(this);
                             var articleId = $e.attr("articleId");
                             if (articleId) {
-                                util.get("../ajax/article/id",{id:articleId}, function (data) {
+                                util.get("../ajax/article/base", {id: articleId}, function (data) {
                                     if (data) {
                                         $e.parent().find(".articleTitle").text(data.articleTitle);
                                         $e.parent().find(".professorName").text(data.professorName || data.organizationName);
@@ -56,6 +56,28 @@ spa_define(function () {
                                     }
                                 }, {});
                                 //     // $e.removeClass("hand-id");
+                            }
+                        });
+                    },
+                    loadLw = function () {
+                        root.find(".hand-id").each(function () {
+                            var $e = $(this);
+                            var articleId = $e.attr("articleId");
+                            if (articleId) {
+                                util.get("/ajax/content/lwCount", {articleId: articleId}, function (data) {
+                                        $e.parent().find(".lw").text(data);
+                                }, {});
+                            }
+                        });
+                    },
+                    loadCol=function () {
+                        root.find(".hand-id").each(function () {
+                            var $e = $(this);
+                            var articleId = $e.attr("articleId");
+                            if (articleId) {
+                                util.get("/ajax/content/countProfessor", {id: articleId,type:3}, function (data) {
+                                        $e.parent().find(".collection").text(data);
+                                }, {});
                             }
                         });
                     },
@@ -77,6 +99,8 @@ spa_define(function () {
                                 tableData.data = allData;
                                 cr.val(tableData.data);
                                 loadContent();
+                                loadLw();
+                                loadCol();
                             }
                         });
                     },
@@ -119,7 +143,273 @@ spa_define(function () {
                             pdg.reload();
                         }
                     })
-                })
+                });
+                var myChart = echarts.init(document.getElementById('total'));
+                var tableData = {data: []},
+                    allData = [];
+                var dayList = function () {
+                    var dayList = [];
+                    var now = new Date();
+                    var day1 = new Date();
+                    day1.setMonth(now.getMonth() - 1);
+                    day1.setDate(1);
+                    var day2 = new Date();
+                    day2.setDate(day2.getDate());
+//                    var s1 = day1.format("yyyy-MM-dd");
+//                    var s2 = day2.format("yyyy-MM-dd");
+//                    console.log(s1, s2);
+                    var days = (day2 - day1) / (24 * 60 * 60 * 1000);
+                    dayList[0] = {time: day1.format("yyyy-MM-dd")};
+                    for (var i = 1; i < days + 1; i++) {
+                        day1.setDate(day1.getDate() + 1);
+                        dayList[i] = {time: day1.format("yyyy-MM-dd")};
+                    }
+                    return dayList;
+                };
+                var loadChart = function () {
+                    $.ajax({
+                        type: "GET",
+                        url: "/ajax/operation/statist/total",
+                        success: function (data) {
+                            allData = dayList();
+                            allData.forEach(function (day) {
+                                var time = (JSON.stringify(day.time)).replace(/\-|^\"|\"$/g, "");
+                                day.pc = 0;
+                                day.app = 0;
+                                day.h5 = 0;
+                                data.data.forEach(function (log) {
+                                    if (time == log.day) {
+                                        if (log.source == 1) {
+                                            day.pc = log.pv;
+                                        }
+                                        if (log.source == 2) {
+                                            day.app = log.pv;
+                                        }
+                                        if (log.source == 3) {
+                                            day.h5 = log.pv;
+                                        }
+                                    }
+                                    day.num = day.pc + day.app + day.h5;
+                                })
+                            });
+                            var colors = ['#003366', "#660099", '#c23531', "#c23531"];
+                            var option = {
+
+                                // color: colors,
+                                title: {
+                                    text: '文章总浏览量',
+                                    subtext: '分渠道'
+                                },
+                                tooltip: {
+                                    trigger: 'axis',
+                                    // formatter:function (params) {
+                                    //     // return params[0].name + '<br/>'
+                                    //     //     + params[3].seriesName + ' : ' + (params[2].value + params[1].value + params[0].value)+ '<br/>'
+                                    //     //     + params[2].seriesName + ' : ' + params[2].value + '<br/>'
+                                    //     //     + params[1].seriesName + ' : ' + params[1].value + '<br/>'
+                                    //     //     + params[0].seriesName + ' : ' + params[0].value + '<br/>';
+                                    //     console.log(params);
+                                    // },
+                                    "axisPointer": {
+                                        "type": "shadow",
+                                        textStyle: {
+                                            color: "#fff"
+                                        }
+
+                                    }
+                                },
+                                legend: {
+                                    // selectedMode:false,
+                                    data: ['PC端', '移动端APP', '移动端H5','合计']
+                                },
+                                xAxis: {
+                                    "type": "category",
+                                    "axisLine": {
+                                        lineStyle: {
+                                            color: '#90979c'
+                                        }
+                                    },
+                                    "splitLine": {
+                                        "show": false
+                                    },
+                                    "axisTick": {
+                                        "show": false
+                                    },
+                                    "splitArea": {
+                                        "show": false
+                                    },
+                                    axisLabel: {
+                                        showMinLabel:true,
+                                        showMaxLabel:true
+                                    },
+                                    "data": allData.map(function (item) {
+                                        return item.time;
+                                    })
+                                },
+                                yAxis: {
+                                    "type": "value",
+                                    "splitLine": {
+                                        "show": false
+                                    },
+                                    "axisLine": {
+                                        lineStyle: {
+                                            color: '#90979c'
+                                        }
+                                    },
+                                    "axisTick": {
+                                        "show": false
+                                    },
+                                    "axisLabel": {
+                                        showMinLabel:true,
+                                        showMaxLabel:true
+                                    },
+                                    "splitArea": {
+                                        "show": false
+                                    }
+                                },
+                                toolbox: {
+                                    show: true,
+                                    feature: {
+                                        dataZoom: {
+                                            yAxisIndex: 'none'
+                                        },
+                                        dataView: {readOnly: false},
+                                        magicType: {type: ['line', 'bar']},
+                                        restore: {},
+                                        saveAsImage: {}
+                                    }
+                                },
+                                dataZoom: [{
+                                    startValue: allData[allData.length - 7].time
+                                }, {
+                                    type: 'inside'
+                                }],
+                                visualMap: {
+                                    top: 10,
+                                    right: 10
+                                },
+                                series: [
+                                    {
+                                        name: '移动端H5',
+                                        type: 'bar',
+                                        stack: '总量',
+                                        "barMaxWidth": 35,
+                                        "barGap": "10%",
+                                        "itemStyle": {
+                                            "normal": {
+                                                "color": "#dd4444",
+                                                "label": {
+                                                    "show": false,
+                                                    "textStyle": {
+                                                        "color": "#fff"
+                                                    },
+                                                    "position": "insideTop",
+                                                    formatter: function(p) {
+                                                        return p.value > 0 ? (p.value) : '';
+                                                    }
+                                                }
+                                            }
+                                        },
+                                        data: allData.map(function (item) {
+                                            return item.h5;
+                                        })
+                                    },
+                                    {
+                                        name: 'PC端',
+                                        type: 'bar',
+                                        stack: '总量',
+                                        "barMaxWidth": 35,
+                                        "barGap": "10%",
+                                        "itemStyle": {
+                                            "normal": {
+                                                "color": "#80F1BE",
+                                                "label": {
+                                                    "show": false,
+                                                    "textStyle": {
+                                                        "color": "#fff"
+                                                    },
+                                                    "position": "insideTop",
+                                                    formatter: function(p) {
+                                                        return p.value > 0 ? (p.value) : '';
+                                                    }
+                                                }
+                                            }
+                                        },
+                                        data: allData.map(function (item) {
+                                            return item.pc;
+                                        })
+                                    }, {
+                                        name: '移动端APP',
+                                        type: 'bar',
+                                        stack: '总量',
+                                        "barMaxWidth": 35,
+                                        "barGap": "10%",
+                                        "itemStyle": {
+                                            "normal": {
+                                                "color": "#fec42c",
+                                                "label": {
+                                                    "show": false,
+                                                    "textStyle": {
+                                                        "color": "#fff"
+                                                    },
+                                                    "position": "insideTop",
+                                                    formatter: function(p) {
+                                                        return p.value > 0 ? (p.value) : '';
+                                                    }
+                                                }
+                                            }
+                                        },
+                                        data: allData.map(function (item) {
+                                            return item.app;
+                                        })
+                                    },
+                                    {
+                                        name: '合计',
+                                        type: 'line',
+                                        stack: '总量',
+                                        symbolSize:7,
+                                        symbol:'circle',
+                                        // areaStyle: {normal: {}},
+                                        // label: {
+                                        //     normal: {
+                                        //         show: true,
+                                        //         position: 'top',
+                                        //         formatter: function (params) {
+                                        //             for (var i = 0, l = option.xAxis.data.length; i < l; i++) {
+                                        //                 if (option.xAxis.data[i] == params.name) {
+                                        //                     // option.series[0].data[i];
+                                        //                     var num = option.series[0].data[i]+option.series[1].data[i]+option.series[2].data[i];
+                                        //                     return '合计：' + num;
+                                        //                 }
+                                        //             }
+                                        //         }
+                                        //
+                                        //     }
+                                        // },
+                                        itemStyle:{
+                                            normal:{
+                                                color:"#dd4444",
+                                                barBorderRadius:0,
+                                                label:{
+                                                    show:true,
+                                                    position:"top",
+                                                    formatter:function (params) {
+                                                        return params.val;
+                                                    }
+                                                }
+                                            }
+                                        },
+                                        data: allData.map(function (item) {
+                                            return item.num;
+                                        })
+                                    }
+                                ]
+                            };
+                            myChart.setOption(option);
+                        }
+                    });
+                };
+                loadChart();
             },
             mainDestory: function () {
             }
